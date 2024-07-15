482cc1e94e3d1b5a0fe016888ab20c42871da814
Jose-Andres-76 <144076576+Jose-Andres-76@users.noreply.github.com>
2024-07-15 16:16:18 -0600

    Home improved, Form finished

--- js/languageChanger.js
+++ js/languageChanger.js
@@ -1,16 +1,53 @@
 // Object containing translations
 const translations = {
     en: {
+        formTitle: "Contact Form",
+        formText: "In the following form you can send us a message with your contact information. We will get back to you as soon as possible.",
+        slogan: "Solid structure, excellent results.",
+        welcome1: "Software Engineer",
+        welcome3: "specialized in backend",
+        welcome4: "A little about me",
+        welcome6: "I am a backend development technician with experience in Java, JavaScript, and .NET, specializing in the design and implementation of efficient and scalable backend systems. My mission is to create high-quality software that brings recognition to the company and satisfaction to our customers. I aspire to be recognized in the technological field for offering agile and innovative solutions, always maintaining a reputation for integrity and responsibility. I am committed to continuous improvement and the adoption of new technologies to deliver exceptional solutions.",
+        welcome8: "My Projects",
+        //carousel
+        slideOneHeader:"SIMEPCI",
+        slideOne:"Private Health Centers Webpage",
+        slideTwoHeader:"AGRIPRO",
+        slideTwo:"Official landing page for AGRIPRO",
+        slideThreeHeader:"Tony's Electronics Manager",
+        slideThree:"Administration system for a Computer sales business",
+        slideFourHeader:"Work Exchange",
+        slideFour:"Work Exchange project for the Costa Rican Ministry of Labor",
         // labels
         labelFN: "Enter your full name:",
         labelEM: "Enter your email:",
         labelCP:"Enter your contact phone:",
         labelA:"Enter your address:",
         labelMSG:"Enter your message:",
-        // labels
-        sendButt: "Send"
+        // buttons
+        sendButt: "Send",
+        // footer
+        footerText:"Solid structure, excellent results.",
+        footerCR:"© 2021 Jose Andres Ramos Espinoza. All rights reserved."
     },
     es: {
+        formTitle: "Formulario de Contacto",
+        formText: "Con el siguiente formulario pueden dejarme su información de contacto, estare al tanto de sus preguntas con respecto a mi trabajo.",
+        slogan: "Estructura firme, resultado excelente.",
+        welcome1: "Ingeniero de Software",
+        welcome3: "con especialidad en backend",
+        welcome4: "Un poco sobre mí",
+        welcome6: "Soy un técnico en desarrollo backend con experiencia en Java, JavaScript y .NET, especializado en el diseño y la implementación de sistemas backend eficientes y escalables. Mi misión es crear software de alta calidad que brinde reconocimiento a la empresa y satisfacción a nuestros clientes. Aspiro a ser reconocido en el ámbito tecnológico por ofrecer soluciones ágiles e innovadoras, manteniendo siempre una reputación de integridad y responsabilidad. Estoy comprometido con la mejora continua y la adopción de nuevas tecnologías para ofrecer soluciones excepcionales.",
+        welcome8: "Mis Proyectos",
+        //carousel
+        slideOneHeader:"SIMEPCI",
+        slideOne:"Página web para Centros de Salud privados",
+        slideTwoHeader:"AGRIPRO",
+        slideTwo:"Landing page oficial de la empresa AGRIPRO",
+        slideThreeHeader:"Administrador de Electrónicos Tony",
+        slideThree:"Sistema de administracion de ventas de un negocio de Computadoras",
+        slideFourHeader:"Bolsa de Trabajo",
+        slideFour:"Proyecto de bolsa de Empleo para el Ministerio de Trabajo de Costa Rica",
         // labels
         labelFN: "Ingrese su nombre completo:",
         labelEM: "Ingrese su correo electrónico:",
@@ -18,12 +55,32 @@ const translations = {
         labelA:"Ingrese su dirección:",
         labelMSG:"Ingrese su mensaje:",
         // buttons
-        sendButt: "Enviar"
+        sendButt: "Enviar",
+        // footer
+        footerText:"Estructura firme, resultado excelente.",
+        footerCR:"© 2021 Jose Andres Ramos Espinoza. Todos los derechos reservados."
     }
 };
 
 // Function to switch language
 function switchLanguage(lang) {
+    document.getElementById('formTitle').textContent = translations[lang].formTitle;
+    document.getElementById('formText').textContent = translations[lang].formText;
+    document.getElementById('slogan').textContent = translations[lang].slogan;
+    document.getElementById('welcome1').textContent = translations[lang].welcome1;
+    document.getElementById('welcome3').textContent = translations[lang].welcome3;
+    document.getElementById('welcome4').textContent = translations[lang].welcome4;
+    document.getElementById('welcome6').textContent = translations[lang].welcome6;
+    document.getElementById('welcome8').textContent = translations[lang].welcome8;
+    //carousel
+    document.getElementById('slideOneHeader').textContent = translations[lang].slideOneHeader;
+    document.getElementById('slideOne').textContent = translations[lang].slideOne;
+    document.getElementById('slideTwoHeader').textContent = translations[lang].slideTwoHeader;
+    document.getElementById('slideTwo').textContent = translations[lang].slideTwo;
+    document.getElementById('slideThreeHeader').textContent = translations[lang].slideThreeHeader;
+    document.getElementById('slideThree').textContent = translations[lang].slideThree;
+    document.getElementById('slideFourHeader').textContent = translations[lang].slideFourHeader;
+    document.getElementById('slideFour').textContent = translations[lang].slideFour;
     // labels
     document.getElementById('labelFN').textContent = translations[lang].labelFN;
     document.getElementById('labelEM').textContent = translations[lang].labelEM;
@@ -32,6 +89,9 @@ function switchLanguage(lang) {
     document.getElementById('labelMSG').textContent = translations[lang].labelMSG;
     // buttons
     document.getElementById('sendButt').textContent = translations[lang].sendButt;
+    //footer
+    document.getElementById('footerText').textContent = translations[lang].footerText;
+    document.getElementById('footerCR').textContent = translations[lang].footerCR;
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
--- js/visibilitySettings.js
+++ js/visibilitySettings.js
@@ -2,20 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('Visibility settings turning ON');
     document.getElementById('btnHome').addEventListener('click', function() {
         document.getElementById('Home').style.display = 'block';
+        document.getElementById('Home2').style.display = 'block';
+        document.getElementById('Home3').style.display = 'block';
         document.getElementById('demo').style.display = 'block';
         document.getElementById('Form').style.display = 'none';
+        document.getElementById('FormIntro').style.display = 'none';
         document.getElementById('logoScreen').style.display = 'none';
     });
     document.getElementById('btnForm').addEventListener('click', function() {
         document.getElementById('Home').style.display = 'none';
+        document.getElementById('Home2').style.display = 'none';
+        document.getElementById('Home3').style.display = 'none';
         document.getElementById('demo').style.display = 'none';
         document.getElementById('Form').style.display = 'block';
+        document.getElementById('FormIntro').style.display = 'block';
         document.getElementById('logoScreen').style.display = 'none';
     });
     document.getElementById('visualizer').addEventListener('click', function() {
         document.getElementById('Home').style.display = 'none';
+        document.getElementById('Home2').style.display = 'none';
+        document.getElementById('Home3').style.display = 'none';
         document.getElementById('demo').style.display = 'none';
         document.getElementById('Form').style.display = 'none';
+        document.getElementById('FormIntro').style.display = 'none';
         document.getElementById('logoScreen').style.display = 'block';
     });
 
